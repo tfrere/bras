d7a76c8419e37f78a3906ed0b8f63477571843a3
poupi <tfrere>
2015-08-13 02:42:35 +0200

    handOnLeapWorks

---
+++ handOnLeap/test/lib/joint.test.js
@@ -0,0 +1,52 @@
+'use strict';
+var assert = require('assert'),
+	fiveStubs = require('./five.stubs'),
+	Joint = require('../../src/lib/joint'),
+	servoStub, joint;
+
+describe('Joint', function() {
+
+	beforeEach(function(){
+		// get the stubbed servo
+		servoStub = fiveStubs.ServoStub();
+		// the constructor will now rely on the stubbed servo
+		joint = new Joint({
+			minPos: 0,
+			maxPos: 180,
+			pin: 9,
+			range: [0,180]
+		});
+	});
+
+	afterEach(function(){
+		servoStub.restore();
+	});
+
+	describe('#move()', function() {
+		it('should move to 90 degrees', function() {
+			joint.move(75);
+			assert.equal(joint.servo.lastMove, 90);
+		});
+		it('should move to 60 degrees', function() {
+			joint.move(25);
+			assert.equal(joint.servo.lastMove, 60);
+		});
+		it('should move to 120 degrees, according to the constraint', function() {
+			joint.move(25, function(pos) {
+				return pos + 100;
+			});
+			assert.equal(joint.servo.lastMove, 120);
+		});
+	});
+
+	describe('#scale()', function() {
+		it('should scale the position to the range of the joint', function() {
+			assert.equal(joint.scale(0), 60);
+			assert.equal(joint.scale(50), 60);
+			assert.equal(joint.scale(75), 90);
+			assert.equal(joint.scale(100), 120);
+			assert.equal(joint.scale(150), 120);
+		});
+	});
+
+});
